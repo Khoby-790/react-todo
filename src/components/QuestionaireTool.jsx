c683321b91b0925aca2579d9b9ac593682077340
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 14:04:05 +0000

    --- Auto-Git Commit ---

--- src/components/QuestionaireTool.jsx
+++ src/components/QuestionaireTool.jsx
@@ -18,7 +18,7 @@ function classNames(...classes) {
 
 const QuestionaireTool = () => {
   return (
-    <div className="w-full lg:w-2/3 border-l-8 border-indigo-500 py-5 px-2 shadow-lg bg-gray-50 my-3 rounded-lg ">
+    <div className="w-full lg:w-2/3 border-l-8 border-indigo-500 pt-5 px-2 shadow-lg bg-gray-50 my-3 rounded-lg ">
       {/* Question and Question Type section */}
       <div className="flex px-4">
         <div className="flex-1">
@@ -210,6 +210,36 @@ const QuestionaireTool = () => {
 
       {/* Settings Section */}
       <div className="border-t flex justify-end border-gray-300">
+        <div className="flex p-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 text-gray-500 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 text-gray-500 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </div>
         <div className="flex"></div>
       </div>
     </div>
