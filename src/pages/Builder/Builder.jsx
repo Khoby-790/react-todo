47bffa52a2bec4cb31dc6c6db2a32339325becfb
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 13:20:17 +0000

    --- Auto-Git Commit ---

--- src/pages/Builder/Builder.jsx
+++ src/pages/Builder/Builder.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Builder = () => {
     return (
-        <div className="px-64 py-5">
-            Hello
+        <div className="flex justify-center py-5">
+            <div className="bg-white shadow rounded border-t-4 border-indigo-600 py-3"></div>
         </div>
     )
 }
