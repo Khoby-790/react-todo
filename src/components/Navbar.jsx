d70b70eb6b0a142d1ed8d64f0cfcbd867ace41d9
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:21:31 +0000

    --- Auto-Git Commit ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,16 @@ const Navbar = () => {
     <div className="bg-white py-5 flex justify-between items-center shadow-md">
       <div className="w-1/4 flex px-4">
         <span className="mr-5">
-          <GiHamburgerMenu size={30} />
+          <GiHamburgerMenu className="text-gray-500" size={30} />
         </span>
         <span className="text-xl">Form Builder</span>
       </div>
       <div className="flex-1">
-        <div className="bg-gray-300 w-full flex  py-2 px-4  rounded-md overflow-hidden">
+        <div className="bg-gray-300 w-full flex  py-2 px-4  rounded-lg overflow-hidden">
           <div>
-              <BsSearch size={30} />
+              <BsSearch className="text-gray-500" size={30} />
           </div>
-          <input className="bg-gray-300" type="text" />
+          <input className="bg-gray-300 flex-1 ml-3 outline-none" type="text" />
         </div>
       </div>
       <div className="w-1/4"></div>
