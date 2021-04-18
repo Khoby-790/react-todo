ea38bff7b98a5a3ccafc97c5b678b0d7c06cf9f0
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:27:49 +0000

    --- Auto-Git Commit ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {BsSearch} from 'react-icons/bs';
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsSearch } from "react-icons/bs";
 
 const Navbar = () => {
   return (
@@ -13,11 +12,14 @@ const Navbar = () => {
         <span className="text-xl">Form Builder</span>
       </div>
       <div className="flex-1">
-        <div className="bg-gray-300 group focus:bg-white focus:shadow-lg hover:bg-white hover:shadow-lg w-full flex  py-2 px-4  rounded-lg overflow-hidden">
+        <div className="bg-gray-300 group transition-all duration-200 transform hover:scale-50 focus:scale-50 focus:bg-white focus:shadow-lg hover:bg-white hover:shadow-lg w-full flex  py-2 px-4  rounded-lg overflow-hidden">
           <div>
-              <BsSearch className="text-gray-500" size={30} />
+            <BsSearch className="text-gray-500" size={30} />
           </div>
-          <input className="bg-gray-300 group-hover:bg-white group-focus:bg-white flex-1 text-gray-500 ml-3 outline-none" type="text" />
+          <input
+            className="bg-gray-300 transition-all duration-200 transform group-hover:bg-white group-focus:bg-white flex-1 text-gray-500 ml-3 outline-none"
+            type="text"
+          />
         </div>
       </div>
       <div className="w-1/4"></div>
