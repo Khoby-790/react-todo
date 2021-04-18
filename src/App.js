48f9aa4a4b3d2937613edeeea7ac148e232a946f
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:42:10 +0000

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter >
       <div className="h-screen overflow-hidden">
         <Navbar />
-        <div className="h-full max-h-full flex-1 bg-red-200 overflow-y-scroll">
+        <div className="h-full max-h-full flex-1  overflow-y-scroll">
           <Route path="/" component={Welcome} />
         </div>
       </div>
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Welcome = () => {
     return (
-        <div className="flex-1 bg-yellow-300 overflow-y-scroll">
-            Welcome
+        <div className="flex-1 h-full flex flex-col">
+            <h2>Hello</h2>
         </div>
     )
 }
