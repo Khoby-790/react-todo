3aa3bcac1abb7ebd347f3bfa5181470ea7924c3c
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:08:56 +0000

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
-
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="bg-white py-2 flex justify-between items-center">
-        <div className="w-1/4">
-          <span>Forms</span>
+        <div className="w-1/4 flex px-4">
+          <span className="mr-5">
+            <GiHamburgerMenu size={30} />
+          </span>
+          <span>Form Builder</span>
         </div>
         <div className="flex-1">
           <div className="bg-gray-300 w-full flex  rounded-md overflow-hidden">
-            <div></div>
+            <div>
+
+            </div>
             <input type="text" />
           </div>
         </div>
