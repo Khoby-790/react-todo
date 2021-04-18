728830ac67ad7077c0714d8e24c3349849bef9d1
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:11:03 +0000

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { Navbar } from "./components";
 import Welcome from "./pages/Welcome";
-import {GiHamburgerMenu} from 'react-icons/gi'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-white py-2 flex justify-between items-center">
-        <div className="w-1/4 flex px-4">
-          <span className="mr-5">
-            <GiHamburgerMenu size={30} />
-          </span>
-          <span>Form Builder</span>
-        </div>
-        <div className="flex-1">
-          <div className="bg-gray-300 w-full flex  rounded-md overflow-hidden">
-            <div>
-
-            </div>
-            <input type="text" />
-          </div>
-        </div>
-        <div className="w-1/4"></div>
-      </div>
+      <Navbar />
       <Route path="/" component={Welcome} />
     </BrowserRouter>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+const Navbar = () => {
+  return (
+    <div className="bg-white py-5 flex justify-between items-center">
+      <div className="w-1/4 flex px-4">
+        <span className="mr-5">
+          <GiHamburgerMenu size={30} />
+        </span>
+        <span className="text-xl">Form Builder</span>
+      </div>
+      <div className="flex-1">
+        <div className="bg-gray-300 w-full flex  rounded-md overflow-hidden">
+          <div></div>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="w-1/4"></div>
+    </div>
+  );
+};
+
+export default Navbar;
