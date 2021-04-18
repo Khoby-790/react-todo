e4c54d6a7a8a7e069a6637952d0545c9a09f0557
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:44:15 +0000

    --- Auto-Git Commit ---

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div className="flex-1 h-full flex flex-col">
-            <h2>Hello</h2>
+            <div className="bg-gray-300 h-64 py-5 flex flex-col px-32">
+                <div className="flex justify-between">
+                    <span>Start a new form</span>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
