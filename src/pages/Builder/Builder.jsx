171d0969c1258cf4a470d1ec89a8670554c0dac8
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 13:24:08 +0000

    --- Auto-Git Commit ---

--- src/pages/Builder/Builder.jsx
+++ src/pages/Builder/Builder.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Builder = () => {
-    return (
-        <div className="flex justify-center py-5">
-            <div className="bg-white px-4 shadow rounded border-t-8 border-indigo-700 py-3 w-2/3">
-                <div className="w-full"></div>
-            </div>
+  return (
+    <div className="flex justify-center py-5">
+      <div className="bg-white px-4 flex flex-col shadow rounded border-t-8 border-indigo-700 py-3 w-2/3">
+        <div className="w-full flex-1">
+          <input type="text" className="text-3xl outline flex-1 font-bold" placeholder="Form Title" />
         </div>
-    )
-}
+        <div>
+            inp
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Builder
+export default Builder;
