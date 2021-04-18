eb2b009b0b285e4b3e8b8ac31d5a219b1abf5c7a
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 13:30:25 +0000

    --- Auto-Git Commit ---

---
+++ src/components/QuestionaireTool.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuestionaireTool = () => {
+    return (
+        <div className="w-full lg:w-2/3 border-l-8 border-indigo-500">
+            
+        </div>
+    )
+}
+
+export default QuestionaireTool
