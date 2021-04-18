068d3298b13d9cebf11b20f697d75cee683eb486
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 14:10:18 +0000

    --- Auto-Git Commit ---

--- src/components/QuestionaireTool.jsx
+++ src/components/QuestionaireTool.jsx
@@ -10,6 +10,7 @@ import {
   PencilAltIcon,
   TrashIcon,
   UserAddIcon,
+  DotsVerticalIcon,
 } from "@heroicons/react/solid";
 
 function classNames(...classes) {
@@ -207,6 +208,7 @@ const QuestionaireTool = () => {
         </div>
       </div>
       {/* Description Section */}
+
       {/* Answers Section */}
       <div className="p-3"></div>
 
@@ -242,8 +244,8 @@ const QuestionaireTool = () => {
             />
           </svg>
         </div>
-        <div className="flex">
-          <Switch.Group as="div" className="flex items-center">
+        <div className="flex items-center">
+          <Switch.Group as="div" className="flex items-center mr-3">
             <Switch.Label as="span" className="ml-3">
               <span className="text-sm font-medium mr-2 text-gray-900">
                 Required
@@ -267,6 +269,100 @@ const QuestionaireTool = () => {
               />
             </Switch>
           </Switch.Group>
+
+          <Menu as="div" className="relative inline-block text-left">
+            {({ open }) => (
+              <>
+                <div>
+                  <Menu.Button className="bg-gray-100 rounded-full flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                    <span className="sr-only">Open options</span>
+                    <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                  </Menu.Button>
+                </div>
+
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items
+                    static
+                    className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Account settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Support
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            License
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <form method="POST" action="#">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              type="submit"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "block w-full text-left px-4 py-2 text-sm"
+                              )}
+                            >
+                              Sign out
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </form>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </>
+            )}
+          </Menu>
         </div>
       </div>
     </div>
