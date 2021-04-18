83c32ccb012616f0987522f56753d4d559ce6616
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 13:34:37 +0000

    --- Auto-Git Commit ---

--- src/components/QuestionaireTool.jsx
+++ src/components/QuestionaireTool.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const QuestionaireTool = () => {
     return (
         <div className="w-full lg:w-2/3 border-l-8 border-indigo-500 py-2 my-3 rounded-lg">
-            
+            <div className="flex">
+                <div className="flex-1"></div>
+            </div>
         </div>
     )
 }
