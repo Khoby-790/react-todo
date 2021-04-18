ab9624bd0209fd662d11246d0881b4ac290d7e8b
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 13:32:32 +0000

    --- Auto-Git Commit ---

--- src/components/QuestionaireTool.jsx
+++ src/components/QuestionaireTool.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const QuestionaireTool = () => {
     return (
-        <div className="w-full lg:w-2/3 border-l-8 border-indigo-500">
+        <div className="w-full lg:w-2/3 border-l-8 border-indigo-500 py-2 my-3 rounded-lg">
             
         </div>
     )
--- src/pages/Builder/Builder.jsx
+++ src/pages/Builder/Builder.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { QuestionaireTool } from "../../components";
 
 const Builder = () => {
   return (
-    <div className="flex justify-center py-5">
+    <div className="flex flex-col items-center py-5">
       <div className="bg-white px-4 flex flex-col shadow rounded border-t-8 border-indigo-700 py-3 w-2/3">
         <div className="flex-1">
           <input
@@ -15,6 +16,7 @@ const Builder = () => {
           <textarea rows={4} type="text" placeholder="Form Description" className="text-gray-500 w-full px-3 outline-none py-2" />
         </div>
       </div>
+      <QuestionaireTool />
     </div>
   );
 };
