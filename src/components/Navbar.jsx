59f2380ea20fc6f7292f3ddbbcf333a6d330d6e0
Khoby-790 <ebaidoo79@gmail.com>
2021-04-18 12:23:38 +0000

    --- Auto-Git Commit ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ const Navbar = () => {
         <span className="text-xl">Form Builder</span>
       </div>
       <div className="flex-1">
-        <div className="bg-gray-300 w-full flex  py-2 px-4  rounded-lg overflow-hidden">
+        <div className="bg-gray-300 group w-full flex  py-2 px-4  rounded-lg overflow-hidden">
           <div>
               <BsSearch className="text-gray-500" size={30} />
           </div>
-          <input className="bg-gray-300 flex-1 ml-3 outline-none" type="text" />
+          <input className="bg-gray-300 flex-1 text-gray-500 ml-3 outline-none" type="text" />
         </div>
       </div>
       <div className="w-1/4"></div>
@@ -25,4 +25,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Nav
