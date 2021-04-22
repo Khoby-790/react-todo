459071c32aaa4a70e47174a94844ef6817d0859b
Khoby-790 <ebaidoo79@gmail.com>
2021-04-22 15:10:07 +0000

    --- Auto-Git Commit ---

--- src/pages/Preview/Preview.jsx
+++ src/pages/Preview/Preview.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import StepWizard from "react-step-wizard";
+import Question from "../../components/Question";
+import { useBuilder } from "../Builder/context/BuilderContext";
+import Header from "../Builder/Header";
 
 const Preview = () => {
+  const { questions } = useBuilder();
   return (
-    <StepWizard className="h-screen w-screen justify-center items-center bg-green-500"></StepWizard>
+    <StepWizard className="h-screen px-36 py-8 w-screen justify-center items-center bg-green-500">
+      <Header />
+      {questions?.map((question, qId) => (
+        <Question key={qId} question={question} />
+      ))}
+    </StepWizard>
   );
 };
 
